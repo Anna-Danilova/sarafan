f022107505bfc06839e9d6724b8cac9a3eb6171e
Anna Danilova <agdanilova@mail.ru>
2024-03-18 15:48:10 +0300

    Адаптив FOOTER до max-width: 320px

--- app/js/main.js
+++ app/js/main.js
@@ -14,3 +14,25 @@ window.addEventListener('click', e => {
     hamburger.classList.remove('header__burger--cross');
   }
 });
+
+//FOOTER-ACCORDEON
+// const footerAccordeonContent = document.querySelectorAll('.footer__content');
+// const footerAccordeonBtn = Array.from(document.querySelectorAll('.footer__title'));
+
+const footerAccordeonContent = Array.from(document.querySelectorAll('.footer__content-accordeon'));
+
+function accordeon(e) {
+  e.preventDefault;
+  let currentContent = e.target.closest('.footer__content-accordeon');
+  let currentAccordeon = e.target.nextElementSibling;
+  currentContent.classList.toggle('footer__content-accordeon--active');
+  if (currentContent.classList.contains('footer__content-accordeon--active')) {
+    currentAccordeon.style.maxHeight = currentAccordeon.scrollHeight + 'px';
+  } else {
+    currentAccordeon.style.maxHeight = 0;
+  }
+}
+
+footerAccordeonContent.forEach(content => {
+  content.addEventListener('click', accordeon);
+});

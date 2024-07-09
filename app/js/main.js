e21c0ba09ef6f05b141505744a0efbbb8980c1ac
Anna Danilova <agdanilova@mail.ru>
2024-07-09 18:17:57 +0300

    Адаптив HEADER до max-width: 320px

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,16 @@
 'use strict';
+//БУРГЕР-МЕНЮ
+const hamburger = document.querySelector('.header__burger');
+const menu = document.querySelector('.header__menu');
+hamburger.addEventListener('click', function () {
+  menu.classList.toggle('header__menu--open');
+  hamburger.classList.toggle('header__burger--cross');
+});
+
+window.addEventListener('click', e => {
+  const target = e.target;
+  if (!target.closest('.header__menu') && !target.closest('.header__burger')) {
+    menu.classList.remove('header__menu--open');
+    hamburger.classList.remove('header__burger--cross');
+  }
+});

6c823c3570edf99504aeab6172d84176fad3b9d2
Anna Danilova <agdanilova@mail.ru>
2024-07-15 11:06:21 +0300

    Прописан ACCORDEON

--- app/js/main.js
+++ app/js/main.js
@@ -14,3 +14,22 @@ window.addEventListener('click', e => {
     hamburger.classList.remove('header__burger--cross');
   }
 });
+
+//АККОРДЕОН
+const accordeonItems = document.querySelectorAll('.accordeon');
+
+function accordeon(e) {
+  e.preventDefault;
+  let currentContent = e.target.closest('.accordeon');
+  let currentAccordeon = e.target.nextElementSibling;
+  currentContent.classList.toggle('accordeon--active');
+  if (currentContent.classList.contains('accordeon--active')) {
+    currentAccordeon.style.maxHeight = currentAccordeon.scrollHeight + 'px';
+  } else {
+    currentAccordeon.style.maxHeight = 0;
+  }
+}
+
+accordeonItems.forEach(content => {
+  content.addEventListener('click', accordeon);
+});
